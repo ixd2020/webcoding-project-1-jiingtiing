31a8eb2aab899607f79b059b4f249cc131f3d1c7
Glitch (heartbreaking-serious-jeep) <none>
2020-12-16 03:16:56 +0000

    👏🛬 Updated with Glitch

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
-/* If you're feeling fancy you can add interactivity 
-    to your site with Javascript */
+var slideIndex = 0;
+showSlides();
 
-// prints "hi" in the browser's dev tools console
-console.log("hi");
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+  setTimeout(showSlides, 3000); // Change image every 3 seconds
+}
